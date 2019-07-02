99f72053230ef6479ef1578d64f3f2901f20a83a
伊藤 太一 <tai.ito@apj4-076.local>
2019-07-02 12:26:23 +0900

    edit setup file

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ mkdir swap backup
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+#ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
